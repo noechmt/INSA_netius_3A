dbea0bdf11ad905132ce7ab52f038a7c10be5ccd
nchaumet <noe.chaumette@insa-cvl.fr>
2023-03-27 15:08:58 +0200

    p2p okay with /quit to quit

--- p2p/libc.c
+++ p2p/libc.c
@@ -8,118 +8,117 @@
 #include <arpa/inet.h>
 #include <pthread.h>
 #include <sys/select.h>
+#include <errno.h>
+#include <string.h>
 
-
-//INADDR_ANY
+// INADDR_ANY
 char name[20];
 int PORT;
 
-void sending();
+int sending();
 void receiving(int server_fd);
 void *receive_thread(void *server_fd);
-
-
 
 void serveur()
 {
     /*printf("Enter your port number:");
     scanf("%d", &PORT);*/
 
-    int server_fd, new_socket, valread;
+    int server_fd;
     struct sockaddr_in address;
-    int k = 0;
 
     // Creating socket file descriptor
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
     {
         perror("socket failed");
+        close(server_fd);
         exit(EXIT_FAILURE);
     }
     // Forcefully attaching socket to the port
 
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
-    //address.sin_addr.s_addr = inet_addr("192.168.206.185");
+    // address.sin_addr.s_addr = inet_addr("192.168.206.185");
     address.sin_port = htons(1234);
 
     struct linger so_linger;
     so_linger.l_onoff = 1;
-    so_linger.l_linger = 30;
-    if(setsockopt(server_fd, SOL_SOCKET, SO_LINGER, &so_linger, sizeof(so_linger)) == -1){
+    so_linger.l_linger = 1;
+    if (setsockopt(server_fd, SOL_SOCKET, SO_LINGER, &so_linger, sizeof(so_linger)) == -1)
+    {
         close(server_fd);
     }
-    //Printed the server socket addr and port
+    // Printed the server socket addr and port
     printf("IP address is: %s\n", inet_ntoa(address.sin_addr));
     printf("port is: %d\n", (int)ntohs(address.sin_port));
 
     if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
     {
         perror("bind failed");
+        close(server_fd);
         exit(EXIT_FAILURE);
     }
     if (listen(server_fd, 5) < 0)
     {
         perror("listen");
+        close(server_fd);
         exit(EXIT_FAILURE);
     }
-    int ch;
     pthread_t tid;
-    pthread_create(&tid, NULL, &receive_thread, &server_fd); //Creating thread to keep receiving message in real time
-    printf("\n*****At any point in time press the following:*****\n1.Send message\n0.Quit\n");
-    printf("\nEnter choice:");
-    do
+    pthread_create(&tid, NULL, &receive_thread, &server_fd); // Creating thread to keep receiving message in real time
+    while (1)
     {
-
-        scanf("%d", &ch);
-        switch (ch)
+        if (sending() < 0)
         {
-        case 1:
-            sending();
             break;
-        case 0:
-            printf("\nLeaving\n");
-            break;
-        default:
-            printf("\nWrong choice\n");
         }
-    } while (ch);
-
+    }
     close(server_fd);
 }
 
-//Sending messages to port
-void sending()
+// Sending messages to port
+int sending()
 {
 
-    char buffer[2000] = {0};
-    //Fetching port number
+    // Fetching port number
     int PORT_server = 1234;
 
-    int sock = 0, valread;
+    int sock = 0;
     struct sockaddr_in serv_addr;
-    char hello[1024] = {0};
+    char buffer_send[1024] = {0};
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
+        close(sock);
         printf("\n Socket creation error \n");
-        return;
+        return -1;
     }
 
     serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = inet_addr("192.168.206.134"); //INADDR_ANY always gives an IP of 0.0.0.0
+    serv_addr.sin_addr.s_addr = inet_addr("192.168.206.185"); // INADDR_ANY always gives an IP of 0.0.0.0
     serv_addr.sin_port = htons(PORT_server);
 
     if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
     {
-        printf("\nConnection Failed \n");
-        return;
+        printf("Waiting for connection\n");
+        sleep(2);
+        return 1;
     }
-
-    send(sock, "hello", sizeof("hello"), 0);
-    printf("\nMessage sent\n");
+    scanf("%s", buffer_send);
+    if (strlen(buffer_send) != 0)
+    {
+        if (strncmp(buffer_send, "/quit", strlen("/quit")) == 0)
+        {
+            return -1;
+        }
+        send(sock, buffer_send, sizeof(buffer_send), 0);
+        bzero(buffer_send, 1024);
+    }
+    printf("Message sent\n");
     close(sock);
+    return 1;
 }
 
-//Calling receiving every 2 seconds
+// Calling receiving every 2 seconds
 void *receive_thread(void *server_fd)
 {
     int s_fd = *((int *)server_fd);
@@ -130,7 +129,7 @@ void *receive_thread(void *server_fd)
     }
 }
 
-//Receiving messages on our port
+// Receiving messages on our port
 void receiving(int server_fd)
 {
     struct sockaddr_in address;
@@ -139,7 +138,7 @@ void receiving(int server_fd)
     int addrlen = sizeof(address);
     fd_set current_sockets, ready_sockets;
 
-    //Initialize my current set
+    // Initialize my current set
     FD_ZERO(&current_sockets);
     FD_SET(server_fd, &current_sockets);
     int k = 0;
@@ -179,7 +178,6 @@ void receiving(int server_fd)
                 }
             }
         }
-
         if (k == (FD_SETSIZE * 2))
             break;
     }
