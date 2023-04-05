905f73f8a57300cb0a60c5557d82ddf9c3c7ba98
thomas2mars <thomasdem2012@gmail.com>
2023-04-05 21:24:39 +0200

    Fix python version

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import os
 import ctypes
 
 # Check of the version of python
-assert sys.version[0:4] == "3.10", "Merci d'utiliser une version de python >= 3.10 !"
+assert sys.version[0:4] == "3.10" or sys.version[0:
+                                                 4] == "3.11", "Merci d'utiliser une version de python >= 3.10 !"
 
 # Check if all the dependencies are installed
 os.system(str(sys.executable) + " -m pip install -r requirements.txt")
