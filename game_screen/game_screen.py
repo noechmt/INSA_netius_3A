4a6d9b651853854388463f52781d69d7e659c6f8
QuentinDOLLEN <quentin.dollen@gmail.com>
2023-04-07 16:31:30 +0200

    Merge de main_network

--- game_screen/game_screen.py
+++ game_screen/game_screen.py
@@ -40,7 +40,7 @@ def game_screen():
     WIDTH_SCREEN, HEIGH_SCREEN = SCREEN.get_size()
     height_land = HEIGH_SCREEN/45
     width_land = WIDTH_SCREEN*sqrt(2)/60
-    SIZE = 75
+    SIZE = 30
 
     # Load new map or existing one with pickle
     file = open("Saves/temp.txt", "r")
---
+++ p2p/Sendpickle.py
@@ -0,0 +1,6 @@
+import threading as thread
+from p2p.socket_python import *
+
+def sendPickleFile() :
+    Server(1235,4)
+    Client()
