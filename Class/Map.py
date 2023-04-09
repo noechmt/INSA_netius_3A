c4d03bbad542b0db4cf5e7f4ae99e919dadbbc9c
tdemars <thomasdem2012@gmail.com>
2023-04-09 23:50:53 +0200

    Another fix parameters

--- Class/Map.py
+++ Class/Map.py
@@ -192,7 +192,7 @@ class Map:  # Un ensemble de cellule
             for cell in self.transaction["cells"]:
                 cell.owner = self.name_user
                 self.wallet -= cell.price
-                encoder.owner(self.name_user, self, self.name_user)
+                encoder.owner(self.name_user, cell, self.name_user)
             self.transaction["Done"] = True
         return self.transaction["Done"]
 
