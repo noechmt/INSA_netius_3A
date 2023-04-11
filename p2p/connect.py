53ec2278a2c433dbec586ca2567dc287cfb0d868
nchaumet <noe.chaumette@insa-cvl.fr>
2023-04-11 19:14:41 +0200

    fix bug connect.py

--- p2p/connect.py
+++ p2p/connect.py
@@ -5,9 +5,9 @@ def socket_init():
    Server(1235,4)
    
    thread_recv = thread.Thread(target=recv_data, args=(Server.socket,))
-   thread_send = thread.Thread(target=send_data, args=("dataDePython",))
+   #thread_send = thread.Thread(target=send_data, args=("dataDePython",))
    thread_recv.start()
-   thread_send.start()
+   #thread_send.start()
 """"
 Server(1235, 4)
 send_data("coucou")"""
