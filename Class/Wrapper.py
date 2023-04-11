5063ffaa778418689f17d83d05c61cbac6d9cbce
QuentinDOLLEN <quentin.dollen@gmail.com>
2023-04-11 17:39:27 +0200

    Avant merge

--- Class/Wrapper.py
+++ Class/Wrapper.py
@@ -153,3 +153,7 @@ class Wrapper:
                 self.map.governors[data["num_player"] -
                                    1].currentCell = self.map.get_cell(data["x"], data["y"])
                 self.map.governors[data["num_player"] - 1].display()
+                
+            case 'quit':
+                self.map.players_online -= 1 
+                print("Player on line = ", self.map.players_online)
